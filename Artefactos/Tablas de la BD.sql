580e74ae5c0d966fd04a9daa752287297f514612
Rapange <h_ero@hotmail.es>
2016-09-30 11:54:09 -0500

    Arreglo de sintaxis en .sql

--- Artefactos/Tablas de la BD.sql
+++ Artefactos/Tablas de la BD.sql
@@ -6,17 +6,14 @@
   account varchar,
   user_type INTEGER,
   "password" varchar
-)
+);
 
-insert into "user"(first_name,last_name,email,account,user_type,"password") values('javier','marin','algo@gmail.com','paracelso',2,'contraceña')
-insert into "user"(first_name,last_name,email,account,user_type,"password") values('francisco','marin','algo@gmail.com','froznar',1,'contraceña')
-
-select * from "proveedor";
-drop table "proveedor";
+insert into "user"(first_name,last_name,email,account,user_type,"password") values('javier','marin','algo@gmail.com','paracelso',2,'contraceña');
+insert into "user"(first_name,last_name,email,account,user_type,"password") values('francisco','marin','algo@gmail.com','froznar',1,'contraceña');
 
 create table "proveedor"(
   id_proveedor SERIAL primary key,
   name_proveedor varchar
-)
+);
 
 insert into "proveedor"(name_proveedor) values('juan');
