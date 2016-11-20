b0e45f04482291231498dbb2543c769868492c76
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2016-11-19 21:53:24 -0500

    #8 agregada funcionalidad para obtener lista de productos

--- lib/repository/sale_repository.dart
+++ lib/repository/sale_repository.dart
@@ -25,7 +25,20 @@ class SaleRepository {
   }
 //General Find Product
   Future<List<Sale>> findAll() async {
-    return (await connection.query('SELECT * FROM "sale" ')).map(mapRowToSale).toList();
+    //return (await connection.query('SELECT * FROM "sale" ')).map(mapRowToSale).toList();
+    return (await connection.query('SELECT * FROM "sale" ')).map(mapRowToSale2).toList();
+  }
+  /*Future<List<Sale>> findByRangeDate(DateTime initialDate, DateTime finalDate) async {
+  }*/
+  Future<List<Product>> findProductsByIdSale(int id) async {//funcion para encontrar lps productos en una determinada venta
+    List<Product> list;
+    List<int> ids = (await connection.query('select id_product * from "sale_product" where id_sale = @id_sale;', {'id_sale': id})).map(mapRowToInt).toList();
+      for(int i = 0; i < ids.length; i++){
+      //Product p;
+      productRepository.find(ids[i]).then((Product p) {list.add(p);});
+      //sale.listProduct.add(p);
+    }
+    return list;//despues debo convertir este future en uno normal
   }
 
 
@@ -40,6 +53,20 @@ class SaleRepository {
       ..priceTotal = row.priceTotal
       ..dateSale =row.dateSale;
 
+  }
+
+  Sale mapRowToSale2(pg.Row row) {
+    Sale s = new Sale()
+      ..id = row.id
+      ..nameBuyer = row.nameBuyer
+      ..DNI = row.DNI
+      ..RUC = row.RUC
+      ..address = row.address
+      ..voucher = row.voucher
+      ..priceTotal = row.priceTotal
+      ..dateSale =row.dateSale;
+    findProductsByIdSale(s.id).then((List<Product> lista){s.listProduct = lista;});
+    return s;
   }
   int mapRowToInt(pg.Row row){
     return row.id_product;
