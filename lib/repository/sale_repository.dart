2b0f00f31697759278424434ee48c952b788c9ec
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2016-11-19 19:14:34 -0500

    #8 Agregado el mapeo de la tabla intermedia

--- lib/repository/sale_repository.dart
+++ lib/repository/sale_repository.dart
@@ -4,17 +4,25 @@ import 'package:di/di.dart';
 import 'package:postgresql/postgresql.dart' as pg;
 import '../config/db_connection.dart';
 import '../model/sale.dart';
+import '../model/product.dart';
+import '../repository/product_repository.dart';
 
 class SaleRepository {
   DbConnection connection;
 
+  ProductRepository productRepository;
   SaleRepository(this.connection);
 
   Future<Sale> find(int id) async {
     Sale sale = (await connection.query('SELECT * FROM "sale" WHERE id = @id', {'id': id})).map(mapRowToSale).first;
+    List<int> ids = (await connection.query('select id_product * from "sale_product" where id_sale = @id_sale;', {'id_sale': id})).map(mapRowToInt).toList();
+    for(int i = 0; i < ids.length; i++){
+      //Product p;
+      productRepository.find(ids[i]).then((Product p) {sale.listProduct.add(p);});
+      //sale.listProduct.add(p);
+    }
     return sale;
   }
-
 //General Find Product
   Future<List<Sale>> findAll() async {
     return (await connection.query('SELECT * FROM "sale" ')).map(mapRowToSale).toList();
@@ -32,6 +40,9 @@ class SaleRepository {
       ..priceTotal = row.priceTotal
       ..dateSale =row.dateSale;
 
+  }
+  int mapRowToInt(pg.Row row){
+    return row.id_product;
   }
 }
 
