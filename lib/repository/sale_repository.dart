82e37c8f16c8dce462617f7b7dcd4fc57f057861
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2016-11-21 20:44:07 -0500

    #8 Correciones y optimizaciones

--- lib/repository/sale_repository.dart
+++ lib/repository/sale_repository.dart
@@ -11,28 +11,28 @@ class SaleRepository {
   DbConnection connection;
 
   ProductRepository productRepository;
-  SaleRepository(this.connection);
+  SaleRepository(this.connection, this.productRepository);
 
   Future<Sale> find(int id) async {
-    Sale sale = (await connection.query('SELECT * FROM "sale" WHERE id = @id', {'id': id})).map(mapRowToSale).first;
-    List<int> ids = (await connection.query('select id_product * from "sale_product" where id_sale = @id_sale;', {'id_sale': id})).map(mapRowToInt).toList();
+    Sale sale = (await connection.query('SELECT * FROM "sale" WHERE id_sale = @id;', {'id': id})).map(mapRowToSale2).first;
+    /*List<int> ids = (await connection.query('select id_product from sale_product where id_sale = @id_sale;', {'id_sale': id})).map(mapRowToInt).toList();
     for(int i = 0; i < ids.length; i++){
       //Product p;
       productRepository.find(ids[i]).then((Product p) {sale.listProduct.add(p);});
       //sale.listProduct.add(p);
-    }
+    }*/
     return sale;
   }
 //General Find Product
   Future<List<Sale>> findAll() async {
     //return (await connection.query('SELECT * FROM "sale" ')).map(mapRowToSale).toList();
-    return (await connection.query('SELECT * FROM "sale" ')).map(mapRowToSale2).toList();
+    return (await connection.query('SELECT * FROM "sale";')).map(mapRowToSale2).toList();
   }
   /*Future<List<Sale>> findByRangeDate(DateTime initialDate, DateTime finalDate) async {
   }*/
   Future<List<Product>> findProductsByIdSale(int id) async {//funcion para encontrar lps productos en una determinada venta
     List<Product> list;
-    List<int> ids = (await connection.query('select id_product * from "sale_product" where id_sale = @id_sale;', {'id_sale': id})).map(mapRowToInt).toList();
+    List<int> ids = (await connection.query('select id_product from "sale_product" where id_sale = @id_sale;', {'id_sale': id})).map(mapRowToInt).toList();
       for(int i = 0; i < ids.length; i++){
       //Product p;
       productRepository.find(ids[i]).then((Product p) {list.add(p);});
@@ -41,35 +41,35 @@ class SaleRepository {
     return list;//despues debo convertir este future en uno normal
   }
 
-
   Sale mapRowToSale(pg.Row row) {
     return new Sale()
-      ..id = row.id
-      ..nameBuyer = row.nameBuyer
-      ..DNI = row.DNI
-      ..RUC = row.RUC
+      ..id = row.id_sale
+      ..nameBuyer = row.namebuyer
+      ..DNI = row.dni
+      ..RUC = row.ruc
       ..address = row.address
       ..voucher = row.voucher
-      ..priceTotal = row.priceTotal
-      ..dateSale =row.dateSale;
+      ..priceTotal = row.pricetotal
+      ..dateSale =row.datesale;
 
   }
 
   Sale mapRowToSale2(pg.Row row) {
     Sale s = new Sale()
-      ..id = row.id
-      ..nameBuyer = row.nameBuyer
-      ..DNI = row.DNI
-      ..RUC = row.RUC
+      ..id = row.id_sale
+      ..nameBuyer = row.namebuyer
+      ..DNI = row.dni
+      ..RUC = row.ruc
       ..address = row.address
       ..voucher = row.voucher
-      ..priceTotal = row.priceTotal
-      ..dateSale =row.dateSale;
+      ..priceTotal = row.pricetotal
+      ..dateSale =row.datesale;
     findProductsByIdSale(s.id).then((List<Product> lista){s.listProduct = lista;});
     return s;
   }
+
   int mapRowToInt(pg.Row row){
-    return row.id_product;
+    return int.parse(row.id_product);
   }
 }
 
