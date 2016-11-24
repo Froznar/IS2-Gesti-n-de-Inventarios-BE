538630d4a36c199827558d255b923d52605a19c1
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2016-11-24 18:24:38 -0500

    Agregados modelo y repositorio para la tabla intermedia sale_product

---
+++ lib/model/sale_product.dart
@@ -0,0 +1,10 @@
+import '../model/product.dart';
+import '../model/sale.dart';
+
+class SaleProduct {
+    int id_sale_product;
+    int id_sale;
+    int id_product;
+    int cantidad;
+    double priceUnit;
+}
