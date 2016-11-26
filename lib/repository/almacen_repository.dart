92e09d48c66db25f9adf32a2ed7b564eb3eb3c5a
paulacamargo25 <paula@inka-labs.com>
2016-11-26 15:56:39 -0500

    #23 agregar almacen repository

---
+++ lib/repository/almacen_repository.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'package:di/di.dart';
+
+import 'package:postgresql/postgresql.dart' as pg;
+import '../config/db_connection.dart';
+import '../model/almacen.dart';
+
+class AlmacenRepository {
+  DbConnection connection;
+
+  AlmacenRepository(this.connection);
+
+  Future<Almacen> find(int id) async {
+    Almacen almacen = (await connection.query('SELECT * FROM "almacen" WHERE id_almacen = @id', {'id': id})).map(mapRowToProveedor).first;
+    return almacen;
+  }
+
+  Future<List<Almacen>> findAll() async {
+    return (await connection.query('SELECT * FROM "almacen" ')).map(mapRowToProveedor).toList();
+  }
+
+  Proveedor mapRowToProveedor(pg.Row row) {
+    return new Almacen()
+      ..id_almacen = row.id_almacen
+      ..name_almacen = row.name_almacen
+      ..map = row.almacen;
+  }
+}
