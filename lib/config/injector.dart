2690beecac1fc41916062f746d19ba2e29fd71ac
DanielaFe7GitHub <daniela.milon@ucsp.edu.pe>
2016-11-27 16:36:07 -0500

    #8 Agraga la Clase CLiente,conexion BD(busqueda e insercion)

--- lib/config/injector.dart
+++ lib/config/injector.dart
@@ -1,9 +1,7 @@
-
 import 'package:di/di.dart';
 
 import 'db_connection.dart';
 import '../repository/user_repository.dart';
-import '../repository/provider_repository.dart';
 import '../repository/user_product_register_repository.dart';
 import '../repository/user_ventas_repository.dart';
 import '../repository/user_administrator_repository.dart';
@@ -11,6 +9,8 @@ import '../repository/product_repository.dart';
 import '../repository/sale_repository.dart';
 import '../repository/sale_product_repository.dart';
 import '../repository/order_repository.dart';
+import '../repository/client_repository.dart';
+
 
 import '../service/user_service.dart';
 import '../service/order_service.dart';
@@ -20,11 +20,11 @@ import '../service/user_administrator_service.dart';
 import '../service/product_service.dart';
 import '../service/sale_service.dart';
 import '../service/sale_product_service.dart';
+import '../service/client_service.dart';
 
 
-import '../service/provider_service.dart';
 import '../rest/user_rest.dart';
-import '../rest/provider_rest.dart';
+import '../rest/order_rest.dart';
 import '../rest/user_product_register_rest.dart';
 import '../rest/user_administrator_rest.dart';
 import '../rest/hello_rest.dart';
@@ -32,7 +32,7 @@ import '../rest/user_ventas_rest.dart';
 import '../rest/product_rest.dart';
 import '../rest/sale_rest.dart';
 import '../rest/sale_product_rest.dart';
-import '../rest/order_rest.dart';
+import '../rest/client_rest.dart';
 
 
 var injector = new ModuleInjector([new Module()
@@ -48,7 +48,8 @@ var injector = new ModuleInjector([new Module()
   ..bind(SaleRepository)
   ..bind(SaleProductRepository)
   ..bind(OrderRepository)
-  ..bind(ProviderRepository)
+  ..bind(ClientRepository)
+
 
 // services
   ..bind(UserService)
@@ -59,13 +60,11 @@ var injector = new ModuleInjector([new Module()
   ..bind(SaleService)
   ..bind(SaleProductService)
   ..bind(OrderService)
-  ..bind(ProviderService)
-
+  ..bind(ClientService)
 
 // REST controllers
   ..bind(HelloRest)
   ..bind(UserRest)
-  ..bind(ProviderRest)
   ..bind(User_Product_Register_Rest)
   ..bind(UserVentasRest)
   ..bind(User_Administrator_Rest)
@@ -73,4 +72,6 @@ var injector = new ModuleInjector([new Module()
   ..bind(SaleRest)
   ..bind(SaleProductRest)
   ..bind(OrderRest)
+  ..bind(ClientRest)
+
 ]);
